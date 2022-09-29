2895fc39a445c439c1de486f84597d5b00db9a31
Oleksìi <bardyshev@gmail.com>
2022-09-29 21:47:30 +0300

    Row component is created

---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Row({ title }) {
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            {/* Container -> posters */}
+        </div>
+    );
+}
+
+export default Row;
