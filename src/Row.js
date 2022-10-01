02a082f3159d76bde816e132b45a70d5c07761f8
Oleksìi <bardyshev@gmail.com>
2022-10-01 20:28:13 +0300

    Posters are fetched from the server

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "./axios";
 
-function Row({ title }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    console.table(movies);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    // console.table(movies);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
+            <div className="row__posters">
+                {/* several row__poster(s) */}
+                {movies.map(movie => (
+                    <img
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
             {/* Container -> posters */}
         </div>
     );
