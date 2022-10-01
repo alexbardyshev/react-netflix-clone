f9d9ae6b7be4614b85c5d339ae146cba9bdb37d5
Oleksìi <bardyshev@gmail.com>
2022-10-01 20:52:30 +0300

    All Rows with movies are added

--- src/Row.js
+++ src/Row.js
@@ -28,6 +28,7 @@ function Row({ title, fetchUrl }) {
                 {/* several row__poster(s) */}
                 {movies.map(movie => (
                     <img
+                        key={movie.id}
                         className="row__poster"
                         src={`${base_url}${movie.poster_path}`} alt={movie.name} />
                 ))}
