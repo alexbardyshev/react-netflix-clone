11aecfb87b1004b53b4f38b58670da46984c7f05
Oleksìi <bardyshev@gmail.com>
2022-10-02 13:36:26 +0300

    Trailer popup is added to Row component

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import axios from "./axios";
 import './Row.css';
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     console.table(movies);
 
@@ -19,6 +23,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }, [fetchUrl]);
 
     // console.table(movies);
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if(trailerUrl) {
+            setTrailerUrl('');
+        }
+        else {
+            movieTrailer(movie?.name || "")
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search);
+                    setTrailerUrl(urlParams.get("v"));
+                })
+                .catch((error) => console.log(error));
+        }
+    }
 
     return (
         <div className="row">
@@ -34,10 +61,11 @@ function Row({ title, fetchUrl, isLargeRow }) {
                             ? movie.poster_path
                             : movie.backdrop_path}`}
                         alt={movie.name}
+                        onClick={() => handleClick(movie)}
                     />
                 ))}
             </div>
-            {/* Container -> posters */}
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
         </div>
     );
 }
